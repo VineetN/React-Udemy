9cc5878f2f5ad2fa90ee6d8a8238b03a875f99b3
VineetN <vineet.nandkishore_ug18@ashoka.edu.in>
2018-12-26 10:54:15 +0530

    new file:   albums/App.js 	new file:   albums/app.json 	new file:   albums/index.js 	new file:   albums/package.json 	new file:   albums/src/components/header.js

---
+++ albums/src/components/header.js
@@ -0,0 +1,31 @@
+// Import libraries
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// Make Component
+const Header = () => {
+  const{ textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.5
+  },
+  textStyle: {
+    fontSize: 40
+  }
+};
+// Make available to other parts of app
+export default Header;
